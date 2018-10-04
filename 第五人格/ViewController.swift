6b43c06a9422485c9beea71d8acc242be6ec747a
User22 <user22@user22de-imac.local>
2018-10-04 14:03:11 +0800

    Initial Commit

---
+++ 第五人格/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  第五人格
+//
+//  Created by User22 on 2018/10/4.
+//  Copyright © 2018 User22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
